bd586007a6a5d74f829f3155f7cc1e7ef89d3fe3
paulinovart <pauline.dierick@gmail.com>
2025-02-20 22:38:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,283 @@
+const questions = [  {
+
+        question:" Qui est Seonghwa ?",
+        answers:[
+            { text: "<img src='images/Seonghwa_1.jpg' class='response_img'>", correct: true},
+            { text: "<img src='images/San_2.png' class='response_img'>", correct: false},
+            { text: "<img src='images/Yeosang_3.png' class='response_img'>", correct: false},
+            { text: "<img src='images/Yunho_3.png' class='response_img'>", correct: false},
+        ]
+    },
+    {
+        question:"Qui est ce membre d'ATEEZ ?",
+        image:"images/Mingi_1.png",
+        answers:[
+            { text: "San", correct: false},
+            { text: "Wooyoung", correct: false},
+            { text: "Mingi", correct: true},
+            { text: "Hongjoong", correct: false},
+        ]
+    },
+
+    {
+        question: "Qui est Yeosang ?",
+        answers: [
+            
+            { text: "<img src='images/Jongho_3.png' class='response_img'>", correct: false },
+            { text: "<img src='images/Wooyoung_2.png' class='response_img'>", correct: false },
+            { text: "<img src='images/Yeosang_1.jpg' class='response_img'>", correct: true },
+            { text: "<img src='images/Yunho_1.png' class='response_img'>", correct: false }
+        ]
+    },
+
+    {
+        question: "Qui est ce membre d'ATEEZ ?",
+        image: "images/Wooyoung_1.png",
+        answers: [
+            { text: "Wooyoung", correct: true },
+            { text: "San", correct: false },
+            { text: "Yunho", correct: false },
+            { text: "Yeosang", correct: false }
+        ]
+    },
+
+  
+
+    {
+        question:"Qui est San ?",
+        answers:[
+            
+            { text: "<img src='images/Seonghwa_2.jpg' class='response_img'>", correct: false},
+            { text: "<img src='images/San_1.png' class='response_img'>", correct: true},
+            { text: "<img src='images/Wooyoung_1.png' class='response_img'>", correct: false},
+            { text: "<img src='images/Mingi_3.png' class='response_img'>", correct: false},
+        ]
+    },
+
+    {
+        question:"Qui est ce membre d'ATEEZ ?",
+        image:"images/Yunho_2.jpg",
+        answers:[
+            { text: "Yeosang", correct: false},
+            { text: "Jongho", correct: false},
+            { text: "Yunho", correct: true},
+            { text: "Seonghwa", correct: false},
+            
+        ]
+    },
+
+    {
+        question:"Qui est Jongho ?",
+        answers:[
+            { text: "<img src='images/Jongho_1.png' class='response_img'>", correct: true},
+            { text: "<img src='images/Yunho_2.jpg' class='response_img'>", correct: false},
+            { text: "<img src='images/Mingi_2.png' class='response_img'>", correct: false},
+            { text: "<img src='images/Yeosang_2.png' class='response_img'>", correct: false},
+        ]
+    },
+
+    {
+        question: "Qui est ce membre d'ATEEZ ?",
+        image: "images/Seonghwa_2.jpg",
+        answers: [
+          
+            { text: "San", correct: false },
+            { text: "Mingi", correct: false },
+            { text: "Jongho", correct: false },
+            { text: "Seonghwa", correct: true },
+        ]
+    },
+
+    {
+        question: "Qui est Mingi ?",
+        answers: [
+           
+            { text: "<img src='images/Seonghwa_3.jpeg' class='response_img'>", correct: false }, 
+            { text: "<img src='images/Mingi_1.png' class='response_img'>", correct: true },
+            { text: "<img src='images/Hongjoong_1.jpg' class='response_img'>", correct: false },
+            { text: "<img src='images/Yeosang_2.png' class='response_img'>", correct: false }
+        ]
+    },
+
+    {
+        question:"Qui est ce membre d'ATEEZ ?",
+        image:"images/Hongjoong_2.png",
+        answers:[
+            
+            { text: "Mingi", correct: false},
+            { text: "Wooyoung", correct: false},
+            { text: "Seonghwa", correct: false},
+            { text: "Hongjoong", correct: true},
+            
+        ]
+    },
+
+    {
+        question:"Qui est Wooyoung ?",
+        answers:[
+            { text: "<img src='images/Jongho_3.png' class='response_img'>", correct: false,},
+            { text: "<img src='images/Hongjoong_3.png' class='response_img'>", correct: false},
+            { text: "<img src='images/Wooyoung_2.png' class='response_img'>", correct: true},
+            { text: "<img src='images/Seonghwa_3.jpeg' class='response_img'>", correct: false},
+        ]
+    },
+    {
+        question:"Qui est ce membre d'ATEEZ ?",
+        image:"images/Yeosang_1.jpg",
+        answers:[
+            { text: "Wooyoung", correct: false},
+            { text: "Jongho", correct: false},
+            { text: "San", correct: false},
+            { text: "Yeosang", correct: true},
+            
+        ]
+    },
+
+    {
+        question: "Qui est Yunho ?",
+        answers: [
+            { text: "<img src='images/Yunho_1.png' class='response_img'>", correct: true },
+            { text: "<img src='images/Mingi_2.png' class='response_img'>", correct: false },
+            { text: "<img src='images/Jongho_2.png' class='response_img'>", correct: false },
+            { text: "<img src='images/Hongjoong_2.png' class='response_img'>", correct: false }
+        ]
+    },
+
+    {
+        question: "Qui est ce membre d'ATEEZ ?",
+        image: "images/Jongho_1.png",
+        answers: [
+             { text: "Seonghwa", correct: false },
+            { text: "Hongjoong", correct: false },
+            { text: "Jongho", correct: true },
+            { text: "Wooyoung", correct: false }
+        ]
+    },
+
+
+    {
+        question: "Qui est Hongjoong ?",
+        answers: [
+            { text: "<img src='images/Hongjoong_3.png' class='response_img'>", correct: true },
+            { text: "<img src='images/Seonghwa_2.jpg' class='response_img'>", correct: false },
+            { text: "<img src='images/San_1.png' class='response_img'>", correct: false },
+            { text: "<img src='images/Mingi_3.png' class='response_img'>", correct: false }
+        ]
+    },
+
+    {
+        question: "Qui est ce membre d'ATEEZ ?",
+        image: "images/San_3.png",
+        answers: [
+            
+            { text: "Seonghwa", correct: false },
+            { text: "San", correct: true },
+            { text: "Wooyoung", correct: false },
+            { text: "Yunho", correct: false }
+        ]
+    },
+
+
+];
+
+const questionElement = document.getElementById("question");
+const Boutonsreponse = document.getElementById("answer_buttons");
+const boutonSuivant = document.getElementById("next_btn");
+
+let IndexQuestionActuelle = 0;
+let score = 0;
+
+function startQuiz(){ //remet les données à 0 pour recommencer le quiz
+    IndexQuestionActuelle = 0;
+    score = 0;
+    boutonSuivant.innerHTML = "Suivant"
+    montrerQuestion();
+}
+
+function montrerQuestion(){
+    CacherQuestionPrecedente();
+    let questionActuelle = questions[IndexQuestionActuelle];
+    let questionNo = IndexQuestionActuelle + 1; // affiche le numéro de la question
+    questionElement.innerHTML = questionNo + ". " + questionActuelle.question; // sélectionne l'élément "question" dans le current question
+
+    const imageContainer = document.getElementById("question_image");
+    if (questionActuelle.image){
+        imageContainer.src = questionActuelle.image;
+        imageContainer.style.display = "block";
+    }else{
+        imageContainer.style.display ="none";
+    }
+
+    questionActuelle.answers.forEach(answer => {// crée les réponses cliquables ! car elles n'existent pas encore
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        Boutonsreponse.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct; // ajoute la donnée selon laquelle la réponse est vrai ou fausse
+        }
+        button.addEventListener("click", selectAnswer);
+        
+    });
+
+
+}
+
+function CacherQuestionPrecedente(){ // cacher la question précédente
+    boutonSuivant.style.display = "none"; //avant qu'on ait répondu à la question, il n'y a pas de boutton suivant !
+    while(Boutonsreponse.firstChild){ // pendant que que answerButtons à un firs child,
+        Boutonsreponse.removeChild(Boutonsreponse.firstChild); // 1. enlever un enfant de "answerButtons" 2. sélectionner le 1er enfant (pour l'enlever)
+    }
+
+}
+
+function selectAnswer(e){
+    const reponseSelectionne = e.target; // la constante "selectedBtn" est égale au boutton sur lequel on a cliqué
+    const isCorrect = reponseSelectionne.dataset.correct == "true"; // c'est correct si : dans les données du boutton séléctionné,la variable boolean "correct" est égal à true
+
+    if(isCorrect){
+        reponseSelectionne.classList.add("correct"); //ajouter la classe correct si c la bonne réponse
+        score++;
+    }else{
+        reponseSelectionne.classList.add("incorrect");// sinon, c que c la mauvaise réponse, alors ajouter la classe incorrect
+    }
+    Array.from(Boutonsreponse.children).forEach(button => { //checque si chaque boutton est vrai , et des qu'il tombe sur un vrai, le met en vert (lui ajoute la classe correct)
+        if(button.dataset.correct == "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true; //desactive le cliquage sur les bouttons
+    });
+    boutonSuivant.style.display = "block"; //montre le boutton suivant
+}
+
+function MontrerScore(){
+    CacherQuestionPrecedente();//cache les questions précédentes
+    const imageContainer = document.getElementById("question_image");
+    if (imageContainer) {
+         imageContainer.style.display = "none";
+    }
+    questionElement.innerHTML =`Score : ${score} sur ${questions.length}`
+    boutonSuivant.innerHTML = "rejouer";
+    boutonSuivant.style.display ="block";
+    imageContainer.style.display ="none";
+}
+
+function MontrerBoutonSuivant(){ // montrer le boutton suivant
+    IndexQuestionActuelle++; //augmente le numéro de la question actuelle
+    if(IndexQuestionActuelle < questions.length){ // si il y a encore des questions apres : les montrer
+        montrerQuestion()
+    }else{
+        MontrerScore(); // sinon, montrer le score
+    }
+}
+
+boutonSuivant.addEventListener("click",()=>{ //si on clique sur le boutton
+    if(IndexQuestionActuelle < questions.length){ //si le numero de la question actuelle est inferieur à la taille de la liste
+        MontrerBoutonSuivant();//fonction qui montre le boutton suivant
+    }else{
+        startQuiz();
+    }
+
+})
+
+startQuiz();
